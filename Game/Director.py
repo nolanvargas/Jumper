be714dab8e021a5eb7ba17667972e8128f72433b
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2022-03-08 09:59:38 -0700

    .

---
+++ Game/Director.py
@@ -0,0 +1,31 @@
+from word import Word
+from jumper import Jumper
+
+class Director:
+
+    def __init__(self):
+        self.jumper = Jumper()
+        self.word = Word()
+        self.display_word = ["_"] * len(self.word)
+        #["_" for i in self.word]
+        self.difficulty = 0
+        self.guesses = []
+        self.is_playing = True
+
+    def update_difficulty(self, difficulty):
+        self.difficulty = difficulty
+
+    def update(self, guess):
+        while self.is_playing:
+            self.guesses.append(guess)
+            word = list(self.word)
+            new_guess = ()
+            if self.jumper.get_health() > 0 and (guess in self.guesses):
+                if guess in word:
+                    
+                    pass
+                else:
+                    self.jumper.update_health()
+            else: self.is_playing = False
+
+    
