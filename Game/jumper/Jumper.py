3334a7855fc043331c3183a62e515eef4331226a
AidanFoushee <aidanfoushee@gmail.com>
2022-03-09 10:51:26 -0700

    finished jumper class

---
+++ Game/jumper/Jumper.py
@@ -0,0 +1,25 @@
+class Jumper:
+
+    def __innit__(self):
+        self._jumper = {
+                        4: ' ___ ',
+                        3: '/___\\',
+                        2: '\\   /',
+                        1: ' \\ / ',
+                    'head': '  o  ',
+                    'arms': ' /|\\ ',
+                    'legs': ' / \\ '
+                        }
+        self._health = 4
+
+
+    def update_health(self):
+        self._jumper.pop(self._health)
+        self._health -= 1
+        if self._health == 0:
+            self._jumper['head'] = 'x'
+        return self._health
+
+
+    def get_health(self):
+        return self._health
