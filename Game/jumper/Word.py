121017567fe3189da2cec8c55911dd2c62276fda
Jesse moore <apachegoboom@gmail.com>
2022-03-09 09:38:04 -0700

    better list creation and added file access.

--- Game/jumper/Word.py
+++ Game/jumper/Word.py
@@ -1,14 +1,25 @@
 class word:
-    def createEasy(self, wordCSV): #3-5 word length
-        minLen = 3
-        maxLen = 5
-    def createNormal(self, wordCSV): #6-8 word length
-        minLen = 6
-        maxLen = 8
-    def createHard(self, wordCSV): #9+ word length
-        minLen = 9
-        maxLen = 99
 
-    def checkLength(self, item, minLen, maxLen): #used in map to collect proper length words.
-        if len(item) >= minLen and len(item) <= maxLen:
-            return item
+    with open("formatted_data.csv", "r") as file:
+        wordCSV = next(file)
+
+    def __init__(self, wordCSV):
+        #Open file, populate a list.
+        self._words = []
+
+
+    def createEasy(self): #3-5 word length
+        return self._filterList(3,5)
+
+    def createNormal(self): #6-8 word length
+        return self._filterList(6,8)
+
+    def createHard(self): #9+ word length
+        return self._filterList(9,99)
+
+    def _filterList(self, minLength, maxLength):
+        list = []
+        for item in self._words:
+            if len(item[1]) >= minLength and len(item[1]) <= maxLength:
+                list.append(item[1])
+        return list
