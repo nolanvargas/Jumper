425a59196e841feb5c24b2c279826f2dd38a515e
AidanFoushee <aidanfoushee@gmail.com>
2022-03-10 09:27:04 -0700

    added comments to jumper

--- Game/jumper/Jumper.py
+++ Game/jumper/Jumper.py
@@ -1,7 +1,7 @@
 class Jumper:
 
     def __innit__(self):
-        self._jumper = {
+        self._jumper = { # this dictionary is so the jumper can be printed to the terminal
                         4: ' ___ ',
                         3: '/___\\',
                         2: '\\   /',
@@ -10,15 +10,15 @@ class Jumper:
                     'arms': ' /|\\ ',
                     'legs': ' / \\ '
                         }
-        self._health = 4
+        self._health = 4 # the health of the jumper
 
 
-    def update_health(self):
+    def update_health(self): # updates the health and the self._jumper dictionary
         self._jumper.pop(self._health)
         self._health -= 1
-        if self._health == 0:
+        if self._health == 0: # when health is equal to 0 this changes the value in the dictionary so the person looks like they're dead
             self._jumper['head'] = 'x'
 
 
-    def get_health(self):
+    def get_health(self): # gets the health and returns it
         return self._health
