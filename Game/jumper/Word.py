227af0fb0835f4fe1703bc949bc3031a15b10193
Jesse moore <apachegoboom@gmail.com>
2022-03-10 09:05:16 -0700

    got list to store info, now ldifficulty needs to return

--- Game/jumper/Word.py
+++ Game/jumper/Word.py
@@ -3,8 +3,8 @@ class Word:
     def __init__(self):
         #Open file, populate a list.
         with open("formatted_data.csv", "r") as file:
-            wordCSV = next(file)
-        self._words = [wordCSV]
+            self._words = file.readlines()[1:]
+            print(self._words)
 
     def createEasy(self): #3-5 word length
         return self._filterList(3,5)
@@ -16,8 +16,8 @@ class Word:
         return self._filterList(9,99)
 
     def _filterList(self, minLength, maxLength):
-        list = []
+        self.diff_list = []
         for item in self._words:
             if len(item[0]) >= minLength and len(item[0]) <= maxLength:
-                list.append(item[0])
-        return list
+                self.diff_list.append(item[0])
+        return self.diff_list
