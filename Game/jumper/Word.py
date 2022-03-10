3ba2a2b8a95c82975c1d804e737cf51e2134801a
Jesse moore <apachegoboom@gmail.com>
2022-03-10 09:15:41 -0700

    Words now work with applied difficulty

--- Game/jumper/Word.py
+++ Game/jumper/Word.py
@@ -1,10 +1,12 @@
 class Word:
 
     def __init__(self):
+        self._wordsProcessed = []
         #Open file, populate a list.
         with open("formatted_data.csv", "r") as file:
-            self._words = file.readlines()[1:]
-            print(self._words)
+            self._wordsRaw = file.readlines()[1:]
+            for item in self._wordsRaw:
+                self._wordsProcessed.append(item.split("," , 1))
 
     def createEasy(self): #3-5 word length
         return self._filterList(3,5)
@@ -17,7 +19,7 @@ class Word:
 
     def _filterList(self, minLength, maxLength):
         self.diff_list = []
-        for item in self._words:
+        for item in self._wordsProcessed:
             if len(item[0]) >= minLength and len(item[0]) <= maxLength:
                 self.diff_list.append(item[0])
         return self.diff_list
